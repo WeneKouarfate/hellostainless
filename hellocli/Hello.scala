5a662d1892a0823eb1743fdb630a4fbb9e53a37c
Wène <wene.kouarfate@epfl.ch>
2026-09-25 03:30:48 +0200

    Working nightly with scala-cli

--- hellocli/Hello.scala
+++ hellocli/Hello.scala
@@ -1,6 +1,9 @@
-//> using scala 3.7.2
-//> using dep ch.epfl.lara::stainless-library:0.9.9.2
-//> using plugin ch.epfl.lara:::stainless-dotty-plugin:0.9.9.2
+//> using scalaOrganization ch.epfl.lara
+//> using repository ivy2Local
+//> using repository m2Local
+//> using scala 3.10.1-RC1-bin-20260903-e1f9361-NIGHTLY
+//> using dep ch.epfl.lara::stainless-library:0.10.2-9-g399c34a
+//> using plugin ch.epfl.lara:::stainless-dotty-plugin:0.10.2-9-g399c34a
 //> using options -Xplugin-require:stainless -P:stainless:verify:true -P:stainless:ghost-elim:true
 
 import stainless.lang._
